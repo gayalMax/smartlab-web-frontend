a78c9448a75b162098a1bb1fa089887d357dc771
Gayal <g.c.dassanayake@gmail.com>
2020-03-13 10:50:13 +0530

    changed folder structure

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import LoginForm from "./Components/LoginForm";
-import CreateAccount from "./Components/adminComponents/CreateAccount";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import RegisterForm from './Components/GuestUser/RegisterForm';
+import CreateAccount from './Components/adminComponents/CreateAccount';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/loginForm" exact component={LoginForm}  />
-        <Route path="/createAccount" exact component={CreateAccount}  />
-        <Route path="/" render={()=><div>404</div>} />
-        <Route path='/loginForm'></Route>
+        <Route path="/register/:registertoken" exact component={RegisterForm} />
+        <Route path="/createAccount" exact component={CreateAccount} />
+        <Route path="/" render={() => <div>404</div>} />
+        <Route path="/loginForm" />
       </Switch>
     </BrowserRouter>
   );
