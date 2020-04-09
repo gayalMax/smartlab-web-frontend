f76d0f5a2d7a3fe575580ae43e4bea9da009bfc4
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2020-04-09 11:35:12 +0530

    Added create role action

--- src/app/redux/actions/AdminAdministration.test.js
+++ src/app/redux/actions/AdminAdministration.test.js
@@ -31,32 +31,64 @@ const responseRolesSync = {
   ]
 };
 
+const responseCreateRole = {
+  id: '1a706073-2bec-4a58-84df-b7bf1ce7682a',
+  name: 'instructor',
+  permissions: ['REGISTRAR']
+};
+
 /* Mock API Configurations */
 
 nock(server.SERVER, { reqheaders: { token: TOKEN } })
   .persist()
   .get(server.SERVER_GET_ROLES_ALL)
-  .reply(200, responseRolesSync);
+  .reply(200, responseRolesSync)
+  .post(server.SERVER_CREATE_ROLE)
+  .reply(200, responseCreateRole);
 
 /* Tests */
 
+// adminAdministrationSyncRoles
 describe('role syncing action creator', () => {
   it('creates BEGIN and SUCCESS when fetching roles has been done', async () => {
-    const expectedActions = [
-      { type: types.ADMIN_ADMINISTRATION_SYNC_ROLES_BEGIN },
-      { type: types.ADMIN_ADMINISTRATION_SYNC_ROLES_SUCCESS, payload: responseRolesSync }
-    ];
     const store = mockStore({});
     await store.dispatch(actions.adminAdministrationSyncRoles(TOKEN));
-    expect(store.getActions()).toEqual(expectedActions);
+    const storeActions = store.getActions();
+    expect(storeActions).toHaveLength(2);
+    expect(storeActions[0]).toHaveProperty('type', types.ADMIN_ADMINISTRATION_SYNC_ROLES_BEGIN);
+    expect(storeActions[1]).toHaveProperty('type', types.ADMIN_ADMINISTRATION_SYNC_ROLES_SUCCESS);
+    expect(storeActions[1]).toHaveProperty('payload', responseRolesSync);
   });
   it('fails when invalid token', async () => {
     const store = mockStore({});
     await store.dispatch(actions.adminAdministrationSyncRoles('invalid'));
     const storeActions = store.getActions();
+    expect(storeActions).toHaveLength(2);
+    expect(storeActions[0]).toHaveProperty('type', types.ADMIN_ADMINISTRATION_SYNC_ROLES_BEGIN);
+    expect(storeActions[1]).toHaveProperty('type', types.ADMIN_ADMINISTRATION_SYNC_ROLES_FAILURE);
+  });
+});
 
-    expect(storeActions[0]).toEqual({ type: types.ADMIN_ADMINISTRATION_SYNC_ROLES_BEGIN });
-    expect(storeActions[1].type).toEqual(types.ADMIN_ADMINISTRATION_SYNC_ROLES_FAILURE);
+// adminAdministrationCreateRole
+describe('create role action creator', () => {
+  it('creates BEGIN and SUCCESS when role created', async () => {
+    const store = mockStore({});
+    await store.dispatch(actions.adminAdministrationCreateRole(TOKEN, 'NAME', null, () => {}));
+    const storeActions = store.getActions();
+    expect(storeActions).toHaveLength(2);
+    expect(storeActions[0]).toHaveProperty('type', types.ADMIN_ADMINISTRATION_CREATE_ROLE_BEGIN);
+    expect(storeActions[1]).toHaveProperty('type', types.ADMIN_ADMINISTRATION_CREATE_ROLE_SUCCESS);
+    expect(storeActions[1]).toHaveProperty('payload', {
+      success: "Role 'NAME' created successfully."
+    });
+  });
+  it('fails when invalid token', async () => {
+    const store = mockStore({});
+    await store.dispatch(actions.adminAdministrationCreateRole('invalid', 'NAME', null, () => {}));
+    const storeActions = store.getActions();
+    expect(storeActions).toHaveLength(2);
+    expect(storeActions[0]).toEqual({ type: types.ADMIN_ADMINISTRATION_CREATE_ROLE_BEGIN });
+    expect(storeActions[1].type).toEqual(types.ADMIN_ADMINISTRATION_CREATE_ROLE_FAILURE);
   });
 });
 
