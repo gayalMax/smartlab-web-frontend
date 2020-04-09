faba3e7ee3b6a1f6463b3fb064f97d32accdaba4
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2020-04-09 22:25:49 +0530

    Added web app testing todo

--- src/test/actions/AuthActions.test.js
+++ src/test/actions/AuthActions.test.js
@@ -1,7 +1,7 @@
 import * as actions from '../../app/redux/actions/AuthActions';
 import * as types from '../../app/redux/actionTypes';
 
-describe('auth action creators', () => {
+describe('sign out action creators', () => {
   it('should emit sign out action', () => {
     expect(actions.authSignOut()).toEqual({
       type: types.AUTH_SIGN_OUT
--- src/test/reducers/AdminAdministrationReducer.test.js
+++ src/test/reducers/AdminAdministrationReducer.test.js
@@ -9,40 +9,8 @@ describe('admin administration reducer', () => {
 
   it('should handle ADMIN_ADMINISTRATION_CREATE_ROLE_BEGIN', () => {
     const action = { type: types.ADMIN_ADMINISTRATION_CREATE_ROLE_BEGIN };
-
-    // No state
     const stateA = reducer({}, action);
     expect(stateA).toEqual({
-      roleCreateLoading: true,
-      roleCreateError: null,
-      roleCreateSuccess: null
-    });
-
-    // Loading state
-    const stateB = reducer(
-      {
-        roleCreateLoading: true,
-        roleCreateError: null,
-        roleCreateSuccess: null
-      },
-      action
-    );
-    expect(stateB).toEqual({
-      roleCreateLoading: true,
-      roleCreateError: null,
-      roleCreateSuccess: null
-    });
-
-    // Success and Error state
-    const stateC = reducer(
-      {
-        roleCreateLoading: true,
-        roleCreateError: 'Demo Error',
-        roleCreateSuccess: 'Demo Success'
-      },
-      action
-    );
-    expect(stateC).toEqual({
       roleCreateLoading: true,
       roleCreateError: null,
       roleCreateSuccess: null
