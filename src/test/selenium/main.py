1107c1cdcd29afd02546c20350afeaf6a14cb359
Gayal <g.c.dassanayake@gmail.com>
2020-05-25 13:09:58 +0530

    seleniumchanges

---
+++ src/test/selenium/main.py
@@ -0,0 +1,17 @@
+import unittest
+import home_page
+
+if __name__ == "__main__":
+    test_classes_to_run = [home_page.TestHomePage, ]
+
+    loader = unittest.TestLoader()
+
+    suites_list = []
+    for test_class in test_classes_to_run:
+        suite = loader.loadTestsFromTestCase(test_class)
+        suites_list.append(suite)
+
+    big_suite = unittest.TestSuite(suites_list)
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    results = runner.run(big_suite)
