5950c03b461b655b292b9e88810c5d64e9f118da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-28 15:00:44 +0000

    Deploy openinventoryorg/web-frontend to github.com/openinventoryorg/web-frontend.git:gh-pages

---
+++ precache-manifest.8aa0025017f2762173e9b96309681645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9195e5ca78823a6f5273fbf54e67262",
+    "url": "/web-frontend/index.html"
+  },
+  {
+    "revision": "769af9cdb2504460d166",
+    "url": "/web-frontend/static/css/main.9661471c.chunk.css"
+  },
+  {
+    "revision": "b05ee4a021e3cd06b1d0",
+    "url": "/web-frontend/static/js/2.4f2c744f.chunk.js"
+  },
+  {
+    "revision": "5c4878da1c0b4be6b5496082a43ea559",
+    "url": "/web-frontend/static/js/2.4f2c744f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769af9cdb2504460d166",
+    "url": "/web-frontend/static/js/main.fdf2a9ec.chunk.js"
+  },
+  {
+    "revision": "78fc2e5d8f01574ab93c",
+    "url": "/web-frontend/static/js/runtime-main.15b27238.js"
+  }
+]);
